04749dd9855c7de2165eee0b2d3163918fede75b
Roman120709 <urall76@yandex.ru>
2022-01-29 17:09:32 +0300

    commit2

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -3,10 +3,11 @@ package ru.netology.sqr;
 public class SQRService {
     public int calculate(int lowValue, int upValue) {
         int i = 0;
-        for (int numer = 10; numer <= 99; numer = numer + 1)
-            if (Math.pow(numer, 2) >= lowValue & Math.pow(numer, 2) <= upValue) {
+        for (int numer = 10; numer <= 99; numer = numer + 1) {
+            if (Math.pow(numer, 2) >= lowValue && Math.pow(numer, 2) <= upValue) {
                 i = i + 1;
             }
+        }
         return i;
     }
 }
